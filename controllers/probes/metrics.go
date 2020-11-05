bcab8fbf7402b87230aad3495b38b849f8217e0f
Jozef Hartinger <jharting@redhat.com>
2020-11-05 18:28:29 +0100

    do not use the pipeline version as a label in metrics

--- controllers/probes/metrics.go
+++ controllers/probes/metrics.go
@@ -11,22 +11,22 @@ var (
 	hostCount = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "cyndi_hosts_total",
 		Help: "Total number of hosts in the given table in the application database",
-	}, []string{"app", "pipeline"})
+	}, []string{"app"})
 
 	inconsistencyRatio = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "cyndi_inconsistency_ratio",
 		Help: "The ratio of inconsistency of data between the source database and the application replica",
-	}, []string{"app", "pipeline"})
+	}, []string{"app"})
 
 	inconsistencyThreshold = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "cyndi_inconsistency_threshold",
 		Help: "The threshold of inconsistency below which the pipeline is considered valid",
-	}, []string{"app", "pipeline"})
+	}, []string{"app"})
 
 	validationFailedCount = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "cyndi_validation_failed_total",
 		Help: "The number of validation iterations that failed",
-	}, []string{"app", "pipeline"})
+	}, []string{"app"})
 
 	refreshCount = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "cyndi_refresh_total",
@@ -39,15 +39,15 @@ func Init() {
 }
 
 func AppHostCount(instance *cyndi.CyndiPipeline, value int64) {
-	hostCount.WithLabelValues(instance.Spec.AppName, instance.Status.PipelineVersion).Set(float64(value))
+	hostCount.WithLabelValues(instance.Spec.AppName).Set(float64(value))
 }
 
 func ValidationFinished(instance *cyndi.CyndiPipeline, threshold int64, ratio float64, isValid bool) {
-	inconsistencyThreshold.WithLabelValues(instance.Spec.AppName, instance.Status.PipelineVersion).Set(float64(threshold) / 100)
-	inconsistencyRatio.WithLabelValues(instance.Spec.AppName, instance.Status.PipelineVersion).Set(ratio)
+	inconsistencyThreshold.WithLabelValues(instance.Spec.AppName).Set(float64(threshold) / 100)
+	inconsistencyRatio.WithLabelValues(instance.Spec.AppName).Set(ratio)
 
 	if !isValid {
-		validationFailedCount.WithLabelValues(instance.Spec.AppName, instance.Status.PipelineVersion).Inc()
+		validationFailedCount.WithLabelValues(instance.Spec.AppName).Inc()
 	}
 }
 
