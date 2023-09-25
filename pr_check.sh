50420a4f00cd380b2af6c790fbf06d573e4d4421
Adam Drew <addrew@redhat.com>
2023-09-25 15:41:42 -0400

    moving things around

--- pr_check.sh
+++ pr_check.sh
@@ -14,9 +14,9 @@ DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 
 #which qemu-user-static
-cat /proc/sys/fs/binfmt_misc/qemu-*
 docker version
 docker buildx version
+cat /proc/sys/fs/binfmt_misc/qemu-*
 
 docker buildx build --platform linux/arm64 -t "${IMAGE}:${IMAGE_TAG}-arm64" .
 docker buildx build --platform linux/amd64 -t "${IMAGE}:${IMAGE_TAG}-amd64" .
