10048cbaf45286ab335930e1ae53f33a8614d9d3
Adam Drew <addrew@redhat.com>
2023-09-25 15:36:36 -0400

    Get some debug info

--- pr_check.sh
+++ pr_check.sh
@@ -13,6 +13,11 @@ fi
 DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 
+which qemu-user-static
+cat /proc/sys/fs/binfmt_misc/qemu-*
+docker version
+docker buildx version
+
 docker buildx build --platform linux/arm64 -t "${IMAGE}:${IMAGE_TAG}-arm64" .
 docker buildx build --platform linux/amd64 -t "${IMAGE}:${IMAGE_TAG}-amd64" .
 
