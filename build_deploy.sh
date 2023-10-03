3da80c31a3f885e9d687beee1e011f0b812c061d
Stephen Adams <stephen.adams@redhat.com>
2023-10-03 10:11:22 -0400

    Update security-compliance tag
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- build_deploy.sh
+++ build_deploy.sh
@@ -4,7 +4,7 @@ set -exv
 
 IMAGE="quay.io/cloudservices/cyndi-operator"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
-SECURITY_COMPLIANCE_TAG="security-compliance"
+SECURITY_COMPLIANCE_TAG="sc-$(date +%Y%m%d)-$(git rev-parse --short=7 HEAD)"
 
 if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
     echo "QUAY_USER and QUAY_TOKEN must be set"
