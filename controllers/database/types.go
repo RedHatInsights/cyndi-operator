154d50f0c014783e2eb40903f7b52bf5721c4e7c
Asa Price <aprice@redhat.com>
2024-10-30 12:05:22 -0400

    Revert "Upgrade pgx to v4; update base image digest (#160)"
    This reverts commit 8400651893857812eeaf1f51afe8ffd4dc4a1076.

--- controllers/database/types.go
+++ controllers/database/types.go
@@ -1,15 +1,14 @@
 package database
 
 import (
-	"github.com/jackc/pgconn"
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx"
 )
 
 type Database interface {
 	Connect() error
 	Close() error
-	RunQuery(query string) (pgx.Rows, error)
-	Exec(query string) (result pgconn.CommandTag, err error)
+	RunQuery(query string) (*pgx.Rows, error)
+	Exec(query string) (result pgx.CommandTag, err error)
 	CountHosts(table string, insightsOnly bool, additionalFilters []map[string]string) (int64, error)
 	GetHostIds(table string, insightsOnly bool, additionalFilters []map[string]string) ([]string, error)
 }
