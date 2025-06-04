69bce3b001f607ec4ae6776fe874dfe6dcd55f23
Asa Price <aprice@redhat.com>
2025-06-04 09:33:18 -0400

    Move last_check_in to end of view and table definitions (#187)

--- controllers/database/app_database.go
+++ controllers/database/app_database.go
@@ -21,7 +21,6 @@ const viewTemplate = `CREATE OR REPLACE VIEW inventory.hosts AS SELECT
 	display_name,
 	created,
 	updated,
-	last_check_in,
 	stale_timestamp,
 	stale_timestamp + INTERVAL '1' DAY * '%[2]s' AS stale_warning_timestamp,
 	stale_timestamp + INTERVAL '1' DAY * '%[3]s' AS culled_timestamp,
@@ -31,7 +30,8 @@ const viewTemplate = `CREATE OR REPLACE VIEW inventory.hosts AS SELECT
 	reporter,
 	per_reporter_staleness,
 	org_id,
-	groups
+	groups,
+	last_check_in
 FROM inventory.%[1]s`
 
 const cullingStaleWarningOffset = "7"
